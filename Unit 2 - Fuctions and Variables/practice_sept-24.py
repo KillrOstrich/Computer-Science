8537dfc410cc7ea98bdf360126ed316051613b94
KillrOstrich <Aschemann06@gmail.com>
2024-09-25 14:21:31 -0500

    animal_survey

---
+++ Unit 2 - Fuctions and Variables/practice_sept-24.py
@@ -0,0 +1,12 @@
+Var = 5              #Decleration - create
+Var = 4              #Assignment - modify
+FA = input("what is your favorite animal?")
+print("your Fav animal is " + FA +"!")
+REA = input("why is your Fav animal " + FA + "?")
+print(REA + " is an interesting reason.")
+sub = input("is it a sub species or just the speices as a whole?")
+print("so " + sub + ", good pick!")
+rea = input("is there another reason you like them?")
+print(rea + ", interesting")
+FIN = input("is there any interesting facts about " + FA + "'s?")
+print("so your Fav animal is a " + FA + " your reason is " + REA + ". you mainly like " + sub + " because " + rea + ". an interesting fact is " + FIN + ".")
