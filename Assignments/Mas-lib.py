56e8d9e4ceda905f615ae45c337bf8eab2e3ade5
KillrOstrich <Aschemann06@gmail.com>
2024-09-26 15:16:10 -0500

    1.05

---
+++ Assignments/Mas-lib.py
@@ -0,0 +1,6 @@
+nou1 = input("give me a noun.") # character 1
+nou2 = input("another one please.") # character 2
+adj1 = input("now a adjective.")
+emo1 = input("give me an emotion")
+emo2 = input("i need a second one")
+ver1 = input("finally a verb")
