12fbfdf760010acbd26ad8858f75838e81f5b6d7
KillrOstrich <Aschemann06@gmail.com>
2024-09-27 14:28:46 -0500

    1.06

--- Assignments/Mas-lib.py
+++ Assignments/Mas-lib.py
@@ -4,3 +4,5 @@ adj1 = input("now a adjective.")
 emo1 = input("give me an emotion")
 emo2 = input("i need a second one")
 ver1 = input("finally a verb")
+
+print("Once upon a time, there was a " + nou1 +  " it was awfully " + adj1 + ". This " + adj1 + " " + nou1 +  " was visiting a " + nou2 + ". Every time " + nou1 + " visited " + nou2 + " it would feel " + emo1 + " and would " + ver1 + " all around. " + nou1 + " would feel " + emo2 + " when leaving " + nou2 + ", but it knew it would be back. THE END")
