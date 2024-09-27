f767f57c822fe4039f3a22638ae5b3536ca3df65
KillrOstrich <Aschemann06@gmail.com>
2024-09-27 15:19:51 -0500

    1.07

---
+++ Unit 2 - Fuctions and Variables/fuctions_sep-27.py
@@ -0,0 +1,16 @@
+x=5
+
+def my_fuction():      #define fuction with name
+    print("hello world!")
+    print("catdog")
+
+def top_5_movies(a,b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+def add(a, b):
+    print(a+b)
+
