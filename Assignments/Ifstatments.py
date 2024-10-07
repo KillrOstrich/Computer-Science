3e2c2dbc26020a3aae27ce20b705dc52ae5dafa9
KillrOstrich <Aschemann06@gmail.com>
2024-10-07 15:19:41 -0500

    1.11

---
+++ Assignments/Ifstatments.py
@@ -0,0 +1,20 @@
+temp = input("whats the temperture in F?\n>")
+temp = int(temp)
+
+if temp >= 80:
+    print(str(temp) + "F is hot!")
+
+if temp < 80:
+    print(str(temp) + "F is not hot...")
+
+
+
+PW = input("password"\n>)
+PW = int(PW)
+
+if PW = skibidi68:
+    print(str(PW) + " is correct")
+
+else:
+    print("incorrect")
+
