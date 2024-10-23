48a375882957592076b26f59d58d398d34ed2dd9
KillrOstrich <Aschemann06@gmail.com>
2024-10-23 15:19:55 -0500

    1.20

--- Assignments/text_adventure.py
+++ Assignments/text_adventure.py
@@ -1,11 +1,10 @@
 def start():   # function to start
     st = input("congrats! you have been given the opertunity to become a super soldier. do you accept? >") #y or n Q for first split
-    ST = st.lower #makes st awnser lower case to fit if statment
 
-    if ST == "yes":
+    if st.lower() == "yes":
         procide() # the continue path
 
-    elif ST == "no":
+    elif st.lower() == "no":
         walk_away() # the first ending
 
     else: # solve errors
@@ -19,12 +18,11 @@ def walk_away(): # first ending where you walk away
 
 def start_over():
     an = input("would you like to startover? >")
-    AN = an.lower
 
-    if AN == "yes":
+    if an.lower() == "yes":
         start()
 
-    elif AN == "no":
+    elif an.lower() == "no":
         print("ok")
         start_over()
     
@@ -33,6 +31,72 @@ def start_over():
         start_over()
 
 def procide(): #continues the story
+    print("you have consented to become a super soldier")
+    an = input("Do you have a peanut allergy? >")
+
+    if an.lower() == "yes":
+        print("Okay, just a heads up you may die.")
+        Death_by_nut()
+   
+    elif an.lower() == "no":
+        print("Good, there could have been complacations.")
+        surgery()
+    
+    else:
+        print("Bruh, y/n simple")
+        procide()
+
+def Death_by_nut():
+    print("everything went fine... until it didn't!")
+    print("you died 1 of 5")
+    print("status: Death by Nut")
+    start_over()
+
+def surgery():
+    print("the procidure went perfectly.")
+    print("you are now given options on your weapon.")
+    wea = input("Sword, Minigun, or Sniper rifle.")
+
+    if wea.lower() == "sword":
+        print("This will end well")
+        sword()
+
+    elif wea.lower() == "minigun":
+        print("Awwwww yeeeeeaaaaah!")
+        minigun()
+
+    elif wea.lower() == "sniper rifle":
+        print("Really. you have enhanced speed and strength, and you choose to shoot people from far away?")
+        print("you are now practiclly unkillable and you become a sniper!?")
+        sniper()
+    
+    else:
+        U_sure()
+
+def sword():
+    print("you fight your first mission, it goes well.")
+    print("the next even better.")
+    print("After a dozen missions you betry the military.")
+    print("you start fighting everyone until your the only one left standing")
+    print("Status: Menice to society")
+    start_over()
+
+def minigun():
     print("")
+
+def U_sure():
+    an = input("Are you sure you want to do this?")
+
+    if an.lower() == "yes":
+        print("Alright Fists it is then.")
+        fists()
+    
+    elif an.lower() == "no":
+        print("ok")
+        surgery()
+    
+    else:
+        print("yes or no")
+        U_sure()
 
 start()
