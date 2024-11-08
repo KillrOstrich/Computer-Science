aa73b9dbb869eae8e4fa4feac47d025344698804
KillrOstrich <Aschemann06@gmail.com>
2024-11-08 15:02:18 -0600

    1.26

---
+++ Unit_4_loops/dictionarys.py
@@ -0,0 +1,3 @@
+#dictionary is a coolection like lists
+#stores data in key-value pairs
+#retreves data quickly with a key insted of an index
---
+++ Unit_4_loops/loop_control.py
@@ -0,0 +1,5 @@
+#changes how loops operate
+#break, continue, and pass
+#break exits a loop early immiditly, program continues where the loop ended
+#continue skips current loop, and moves to the next iderrations
+#pass does nothing, its a place holder
